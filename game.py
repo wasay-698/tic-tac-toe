7d2d0e0743a4d19d8b9bd6ef6f2481a7155affad
wasay-698 <wasaytariq59@gmail.com>
2024-09-30 22:52:09 +0500

    new msg

--- game.py
+++ game.py
@@ -2,4 +2,9 @@ class Tictactoe:
     def __init__(self):
         self.board = [' ' for _ in range(9)] #we will use a single list to repl 3x3 board
         self.current(winner) = None
-        
+
+    def print_board(self):
+        #this is just getting the rows
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + '| ' + ' | ' )
+            
